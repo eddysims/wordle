4ac8a440b902797ca2082dc6d55ee6c100dba8b3
eddysims <eddysimsweb@gmail.com>
2023-07-20 22:35:46 -0600

    adds the box colors

--- src/components/LetterBox.tsx
+++ src/components/LetterBox.tsx
@@ -1,12 +1,21 @@
+import { clsx } from "clsx";
+
+export type LetterStatus = "unused" | "used" | "correct"
+
 interface LetterBoxProps {
   letter?: string;
-  // status: 'empty' | 'close' | 'success'
+  status?: LetterStatus;
 }
 
-export function LetterBox({ letter }: LetterBoxProps) {
-  return (
-    <div className="border border-neutral-300 flex items-center justify-center h-10 w-10 rounded">
-      {letter && letter[0]}
-    </div>
+export function LetterBox({ letter, status }: LetterBoxProps) {
+  const classNames = clsx(
+    "border border-neutral-300 flex items-center justify-center h-10 w-10 rounded",
+    {
+      "bg-white": status === "unused",
+      "bg-yellow-100": status === "used",
+      "bg-green-100": status === "correct",
+    }
   );
+
+  return <div className={classNames}>{letter && letter[0]}</div>;
 }
